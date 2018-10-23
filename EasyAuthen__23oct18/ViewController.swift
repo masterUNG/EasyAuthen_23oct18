0a7491fd591610f174f3b094fd1cccad49010555
masterUNG <phrombutr@gmail.com>
2018-10-23 14:01:46 +0700

    Get Value From TextField

--- EasyAuthen__23oct18/ViewController.swift
+++ EasyAuthen__23oct18/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    Explicit
+    var userString: String = ""
+    var passwordString: String = ""
+    
+    
+    
 
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBAction func signInButton(_ sender: Any) {
+        
+//        Get Value From TextField
+        userString = userTextField.text!
+        passwordString = passwordTextField.text!
+        
+//        Show Log
+        print("user ==> \(userString)   password ==> \(passwordString)")
+        
+        
+    }   // signIn Function
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
