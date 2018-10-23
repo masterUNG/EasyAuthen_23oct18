284839a558813162b8d2accd8b8b6089b56da302
masterUNG <phrombutr@gmail.com>
2018-10-23 15:18:31 +0700

    Ui Stgn Up OK

--- EasyAuthen__23oct18/SignUpViewController.swift
+++ EasyAuthen__23oct18/SignUpViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBAction func cloudUpLoadButton(_ sender: Any) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
